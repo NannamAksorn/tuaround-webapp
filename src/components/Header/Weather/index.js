1776356674e2d9e697f36b235ba887b55966d8af
Ratthamontree Burimas <bank23232525@gmail.com>
2020-03-23 00:10:07 +0700

    ui 5

--- src/components/Header/Weather/index.js
+++ src/components/Header/Weather/index.js
@@ -1,27 +1,23 @@
-import React from 'react';
+import React from "react";
 
 //import lottie from 'lottie-web';
-import Lottie from 'react-lottie';
-import * as animationData from './pinjump.json'
+import Lottie from "react-lottie";
+import * as animationData from "./pinjump.json";
 //var currentPath = require("./weather/cloudy.json");
 //var currentPath = require("./weather/clear-night.json");
 
-
-
 //const Weather = () =>
 class Weather extends React.Component {
-
-
   constructor(props) {
     super(props);
     this.state = {
-      currentPath: require(`./weather/clear-night.json`),
+      currentPath: require(`./weather/clear-night.json`)
     };
   }
   async fetchWeather() {
-    const weatherIcon = document.getElementById('MyWeatherIcon');
-    const weatherTempEl = document.getElementById('MyWeatherTemp');
-    const weatherPrecipEl = document.getElementById('MyWeatherPrecip');
+    const weatherIcon = document.getElementById("MyWeatherIcon");
+    const weatherTempEl = document.getElementById("MyWeatherTemp");
+    const weatherPrecipEl = document.getElementById("MyWeatherPrecip");
 
     // let lottieWeatherIcon = lottie.loadAnimation({
     //   container: document.getElementById('MyWeatherIcon'),
@@ -37,40 +33,42 @@ class Weather extends React.Component {
     //const res = await fetch('/api/weather');
     //const res = await fetch('http://tuaround.com:4435/api/weather');
     let header = new Headers({
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'multipart/form-data'
+      //'Access-Control-Allow-Origin': '*',
+      "Content-Type": "application/json"
     });
     //let opt = Object.assign({}, defaultOptions, options); //将默认的参数和传过来的合并在一起
     let sentData = {
       method: "GET",
-      mode: 'cors',
-      header: header,
+      //mode: 'cors',
+      crossDomain: true,
+      header: header
       //body: opt.body || ''
     };
-    const res = await fetch('https://api.darksky.net/forecast/56d11b10e025f7cd93cc31496fba4cb6/14.0717,%20100.60205?units=si', sentData);
+    const res = await fetch(
+      "https://api.darksky.net/forecast/56d11b10e025f7cd93cc31496fba4cb6/14.0717,%20100.60205?units=si",
+      sentData
+    );
     console.log(res);
     const data = await res.json();
     console.log(data);
-    console.log(document.getElementById('MyWeatherTemp'))
-    if (document.getElementById('MyWeatherTemp')) {
-      console.log('sss')
-      document.getElementById('MyWeatherTemp').innerText = `${data.currently.apparentTemperature.toFixed(
-        0,
-      )}°C`;
-
+    console.log(document.getElementById("MyWeatherTemp"));
+    if (document.getElementById("MyWeatherTemp")) {
+      console.log("sss");
+      document.getElementById(
+        "MyWeatherTemp"
+      ).innerText = `${data.currently.apparentTemperature.toFixed(0)}°C`;
     }
-    if (document.getElementById('MyWeatherPrecip')) {
-      document.getElementById('MyWeatherPrecip').innerText = `${(
+    if (document.getElementById("MyWeatherPrecip")) {
+      document.getElementById("MyWeatherPrecip").innerText = `${(
         data.currently.precipProbability * 100
       ).toFixed(0)}%`;
-
     }
-    if (document.getElementById('MyWeatherIcon')) {
-      console.log("set icon")
+    if (document.getElementById("MyWeatherIcon")) {
+      console.log("set icon");
       // if (document.getElementById('MyWeatherIcon').innerHTML) {
       //   lottieWeatherIcon.destroy();
       // }
-      console.log(data.currently.icon)
+      console.log(data.currently.icon);
       // lottieWeatherIcon = lottie.loadAnimation({
       //   container: document.getElementById('MyWeatherIcon'),
       //   renderer: 'svg',
@@ -81,16 +79,17 @@ class Weather extends React.Component {
       //     clearCanvas: true,
       //   },
       // });
-      this.setState({ currentPath: require(`./weather/${data.currently.icon}.json`) })
+      this.setState({
+        currentPath: require(`./weather/${data.currently.icon}.json`)
+      });
       //currentPath = require(`./weather/${data.currently.icon}.json`);
     }
   }
 
   autofetchWeather() {
-    console.log("autofetchWeather")
+    console.log("autofetchWeather");
     this.fetchWeather();
     setInterval(this.fetchWeather, 1000 * 60 * 60);
-
   }
   componentDidMount() {
     this.autofetchWeather();
@@ -103,56 +102,47 @@ class Weather extends React.Component {
       animationData: this.state.currentPath,
       //path: `./weather/clear-night.json`,
       rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice'
+        preserveAspectRatio: "xMidYMid slice"
       }
     };
 
     return (
       <>
-        <div className="forecast-container" onFinish={this.autofetchWeather()} >
+        <div className="forecast-container" onFinish={this.autofetchWeather()}>
           {/******  replace this **********/}
-          <div className="temp-icon">
-
-            <div id="MyWeatherIcon" >
-
-              <Lottie options={defaultOptions}
-                height={80}
-                width={80}
-                isStopped={false}
-                isPaused={false} />
-            </div>
-
+          <div id="MyWeatherIcon">
+            <Lottie
+              options={defaultOptions}
+              isStopped={false}
+              isPaused={false}
+            />
           </div>
         </div>
 
-        <div className="weather-container" >
-          <div className="temp-container">
-            <div className="temp-icon">
-              <img
-                className="mr-4"
-                src={require('./temp.svg')}
-                alt="temp"
-                width="25px"
-              />
+        <div className="weather-container">
+          <div className="sub-container2">
+            <div className="temp-container">
+              <div className="temp-icon">
+                <img src={require("./temp.svg")} alt="temp" />
+              </div>
+              <div id="MyWeatherTemp" className="temp-info">
+                <p>30°C</p>
+              </div>
             </div>
-            <div id="MyWeatherTemp" className="temp-info" >30°C</div>
-          </div>
-          <div className="rain-container">
-            <div className="rain-icon">
-              <img
-                className="mr-4"
-                src={require('./raining.svg')}
-                alt="raining"
-                width="25px"
-              />
+            <div className="rain-container">
+              <div className="rain-icon">
+                <img src={require("./raining.svg")} alt="raining" />
+              </div>
+              <div id="MyWeatherPrecip" className="rain-info">
+                {" "}
+                <p>25%</p>
+              </div>
             </div>
-            <div id="MyWeatherPrecip" className="rain-info"> 25%</div>
           </div>
         </div>
       </>
     );
-
   }
-};
+}
 
 export default Weather;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './styles.scss';
-import DateTime from '@/components/Header/DateTime';
-import Weather from '@/components/Header/Weather/index';
+import React from "react";
+import "./styles.scss";
+import DateTime from "@/components/Header/DateTime";
+import Weather from "@/components/Header/Weather/index";
 
 const Header = () => {
   return (
@@ -10,7 +10,7 @@ const Header = () => {
       {/* Left side */}
       <div className="logo-container">
         <div className="logo">
-          <img src={'/img/tu-logo.png'} alt="tu-logo" />
+          <img src={"/img/tu-logo.png"} alt="tu-logo" />
         </div>
         <div className="title">
           <h3>Thammasat University Transportation</h3>
@@ -19,13 +19,15 @@ const Header = () => {
 
       {/* Right side */}
       <div className="right-container">
-        <div className="info-time-container">
-          {/* DATE TIME */}
-          <DateTime />
-        </div>
-        <div className="info-weather-container">
-          {/* WEATHER */}
-          <Weather />
+        <div className="sub-container">
+          <div className="info-time-container">
+            {/* DATE TIME */}
+            <DateTime />
+          </div>
+          <div className="info-weather-container">
+            {/* WEATHER */}
+            <Weather />
+          </div>
         </div>
       </div>
     </div>
